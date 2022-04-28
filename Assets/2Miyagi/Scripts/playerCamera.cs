a35e98b445c81d418df131a7d597fcbfe25e83eb
miyagirei <ra.mu.umy243@gmail.com>
2022-04-28 19:01:39 +0900

    プレイヤーの移動とカメラを作成

---
+++ Assets/2Miyagi/Scripts/playerCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour
+{
+    public Transform yAxis;
+    public Transform xAxis;
+    public float xSence;
+    public float ySence;
+    public float limitXAxizAngle = 30;
+    private Vector3 mXAxiz;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        mXAxiz = xAxis.localEulerAngles;
+    }
+    private void cameraFPS()
+    {
+        float xCamera = Input.GetAxis("Mouse X") * -xSence * Time.deltaTime;
+        yAxis.transform.Rotate(0, -xCamera, 0);
+
+        float yCamera = Input.GetAxis("Mouse Y") * -ySence * Time.deltaTime;
+        var x = mXAxiz.x + yCamera;
+        if (x >= -limitXAxizAngle && x <= limitXAxizAngle)
+        {
+            mXAxiz.x = x;
+            xAxis.localEulerAngles = mXAxiz;
+        }
+    }
+    void Update()
+    {
+        cameraFPS();
+    }
+}
---
+++ Assets/2Miyagi/Scripts/playerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float mainSpeed = 3.0f;
+    public float mainJampSpeed = 8.0f;
+    public float gravity = 20.0f;
+    void Start(){
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * mainSpeed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = mainJampSpeed;
+            }
+        }
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
