71f2aa43508d705bf4914e5c309b58f5a55c8728
BAKUSOUMARU <bakusoumaro@gmail.com>
2022-07-20 12:04:55 +0900

    prefabのscriptの調整

--- Assets/3Wada/script/hammercontroller.cs
+++ Assets/3Wada/script/hammercontroller.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class hammercontroller : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Destroy(this.gameObject);
             GameManager.instance._hammer++;
             Debug.Log(GameManager.instance._hammer);
-        }
+        }   
     }
 }
